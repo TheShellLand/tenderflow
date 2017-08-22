1a8b5a4250572bfa588028129b7a7edcd7d5460e
Eric Jaw <naisanza@gmail.com>
2017-08-21 21:22:42 -0400

    Working 2 layer LSTM-RNN

--- keras/lstm_101.py
+++ keras/lstm_101.py
@@ -30,6 +30,8 @@ load_checkpoint = ''
 def char2vec(dataset):
     """Convert dataset into training data
 
+    Total Vocab, is also the number of classes
+
     x: x_patterns, seq_length, features
     y: one hot encoding
 
@@ -63,15 +65,13 @@ def char2vec(dataset):
         y.append(char_to_int[sequence_out])
 
     n_patterns = x.__len__()
-    time_steps = n_patterns
-    samples = n_patterns
 
     print('[*] Total Patterns', n_patterns)
 
     # The input sequences that might vary in length between 1 and max_len and
     # therefore require zero padding. Here, we use left-hand-side
     # (prefix) padding with the Keras built in pad_sequences() function.
-    x = pad_sequences(x, maxlen=seq_length, dtype='float32')
+    x = pad_sequences(x, maxlen=seq_length, dtype='float32')        # shape: (163717, 100)
 
     # We need to reshape the NumPy array into a format expected by
     # the LSTM networks, that is [samples, time step, features].
@@ -84,7 +84,11 @@ def char2vec(dataset):
     # LSTM input must be 3D, the structure is [samples, timesteps, features].
     # If you one hot encode, then the number of labels (length of binary vectors)
     # is the number of features.
-    x = numpy.reshape(x, (n_patterns, seq_length, features))
+    # ;
+    # (number of samples, number of timesteps, number of features)
+    samples = n_patterns
+    timesteps = seq_length
+    x = numpy.reshape(x, (samples, timesteps, features))        # shape: (163717, 100, 1)
 
     # Once reshaped, we can then normalize the input integers to the
     # range 0-to-1, the range of the sigmoid activation functions used
@@ -96,8 +100,9 @@ def char2vec(dataset):
     # such, we can convert the output (y) to a one hot encoding, using the
     # Keras built-in function to_categorical().
     y = np_utils.to_categorical(y)
+    output = y.shape[1]
 
-    return x, y, n_chars, vocab_size, n_patterns, char_to_int, int_to_char
+    return x, y, n_chars, vocab_size, samples, timesteps, output, char_to_int, int_to_char
 
 
 def word2vec(dataset):
@@ -135,7 +140,7 @@ def unsupervised(x):
     pass
 
 
-def semisuperfized(more_x, less_y):
+def semisupervized(more_x, less_y):
     """
     Problems where you have a large amount of input data (X) and only
     some of the data is labeled (Y) are called semi-supervised learning
@@ -156,6 +161,40 @@ def semisuperfized(more_x, less_y):
     :param more_x:
     :param less_y:
     :return:
+    """
+    pass
+
+
+def timesteps_dimension():
+    """
+    - timesteps: usually indicate the length of your sequence (each sample
+    should have same length, or you should do some paddings of your input
+    to have the same length)
+
+    - dimension: usually indicate the number of units in a specific layer.
+    for e.g. if your word embedding is 100 dimension, then you just set
+    this param as 100.
+    """
+    pass
+
+
+def time_dimension():
+    """
+    For a feed-forward network, your input has the shape
+    (number of samples, number of features).
+
+    With an LSTM/RNN, you add a time dimension, and your input shape becomes
+    (number of samples, number of timesteps, number of features).
+    This is in the documentation.
+
+    So if your feature dimension is 5, and you have 2 timesteps, your input
+    could look like:
+
+    [ [[1,2,3,4,5], [2,3,4,5,6]], [[2,4,6,8,0], [9,8,7,6,5]] ]
+
+    Your output shape depends on how you configure the net. If your LSTM/RNN
+    has return_sequences=False, you'll have one label per sequence; if you set
+    return_sequences=True, you'll have one label per timestep.
     """
     pass
 
@@ -181,10 +220,11 @@ batch_size = 128
 seq_length = 100        # input_length
 features = 1            # input_dim  # input vector shape(?)
 epochs = 1000000
-initial_epoch = 121
+initial_epoch = 0
 
-x_train, y_train, n_chars, vocab_size, n_patterns, char_to_int, int_to_char = char2vec(ds)
-test = char2vec(t_ds)
+x_train, y_train, n_chars, vocab_size, samples, timesteps, output, char_to_int, int_to_char = char2vec(ds)
+x, y = x_train, y_train
+x_val, y_val = x, y
 
 model = Sequential()
 
@@ -194,37 +234,31 @@ model = Sequential()
 # Example: reshape X to be [samples, time steps, features]
 # Example: reshape X to be [samples, iterations, input vector]
 # Example: (number_of_sequences, length_of_sequence, number_of_features)
-# model.add(LSTM(64, input_shape=(10,64)))
+# Documentation: (number of samples, number of timesteps, number of features)
+# Example: model.add(LSTM(64, input_shape=(10,64)))
 
 # Dense: 2D, input_shape=(batch_size, input_dim)
 
-model.add(LSTM(vocab_size, input_shape=(seq_length, features), return_sequences=True))
-print(model.output_shape)
-model.add(Dropout(0.5))
-print(model.output_shape)
-model.add(LSTM(vocab_size, return_sequences=True))
-print(model.output_shape)
-model.add(Flatten())
-print(model.output_shape)
-model.add(Dense(y_train.shape[1]))
-print(model.output_shape)
-model.add(Activation('relu'))
+model.add(LSTM(256, input_shape=(timesteps, features), return_sequences=True))  # (None, 100, 256)
 print(model.output_shape)
 
-# (None, 61)        <-- LSTM(vocab_size, input_shape=(seq_length, features))
+model.add(LSTM(256, return_sequences=True))    # (None, 100, 256)
+print(model.output_shape)
 
-# (None, 100, 61)
-# (None, 100, 61)
-# (None, 100, 61)
-# (None, 100, 60)
-# (None, 100, 60)
+model.add(Dropout(0.5)) # (None, 256)
+print(model.output_shape)
 
-# (None, 100, 61)
-# (None, 100, 61)
-# (None, 100, 61)
-# (None, 6100)      <-- Flatten
-# (None, 60)
-# (None, 60)
+model.add(Flatten())      # Flattens 3D -> 2D   # ValueError: Error when checking target: expected activation_1 to have 3 dimensions, but got array with shape (163717, 60)
+print(model.output_shape)
+
+model.add(Dense(output))    # (None, 60)
+print(model.output_shape)
+
+model.add(Activation('relu'))   # (None, 60)
+print(model.output_shape)
+
+
+
 
 
 model.compile(loss='categorical_crossentropy',
@@ -239,40 +273,21 @@ if isfile(load_checkpoint):
 
 
 # Updates happen after each batch
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          callbacks=callbacks_list,
-          validation_data=(test[0], test[1]),
-          shuffle=True,
-          initial_epoch=initial_epoch)
+history = model.fit(x, y,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    callbacks=callbacks_list,
+                    validation_data=(x_val, y_val),
+                    shuffle=False,
+                    initial_epoch=initial_epoch)
 
 
-scores = model.evaluate(x_train, y_train, verbose=1)
+score, acc = model.evaluate(x, y, batch_size=batch_size, verbose=1)
+print(score, acc)
 
 
-# for pattern in dataX:
-#     x = numpy.reshape(pattern, (1, len(pattern), 1))
-#     x = x / float(len(alphabet))
-#     prediction = model.predict(x, verbose=0)
-#     index = numpy.argmax(prediction)
-#     result = int_to_char[index]
-#     seq_in = [int_to_char[value] for value in pattern]
-#     print(seq_in, "->", result)
 
-
-# for i in range(20):
-# 	pattern_index = numpy.random.randint(len(dataX))
-# 	pattern = dataX[pattern_index]
-# 	x = pad_sequences([pattern], maxlen=max_len, dtype='float32')
-# 	x = numpy.reshape(x, (1, max_len, 1))
-# 	x = x / float(len(alphabet))
-# 	prediction = model.predict(x, verbose=0)
-# 	index = numpy.argmax(prediction)
-# 	result = int_to_char[index]
-# 	seq_in = [int_to_char[value] for value in pattern]
-# 	print seq_in, "->", result
 
 
 
