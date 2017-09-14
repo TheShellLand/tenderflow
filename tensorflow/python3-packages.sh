5459f901c47784911b6f9e2a04c97fc878bccf89
Eric Jaw <naisanza@gmail.com>
2017-09-13 19:14:32 -0700

    Added separate install commands for virtual env

--- tensorflow/python3-packages.sh
+++ tensorflow/python3-packages.sh
@@ -9,3 +9,9 @@ sudo -H  pip3 install --upgrade tensorflow-gpu
 sudo -H  pip3 install --upgrade tensorboard
 sudo -H  pip3 install --upgrade keras
 sudo -H  pip3 install --upgrade h5py
+
+pip3 install --upgrade pip
+pip3 install --upgrade tensorflow
+pip3 install --upgrade tensorboard
+pip3 install --upgrade keras
+pip3 install --upgrade h5py
